9d4e033086182a7183d96011e1b2cadd5aad0b7a
Marek Klement <marek@klement.cz>
2022-12-15 15:26:54 +0100

    added missing routing

--- src/main/kotlin/cz/klement/plugins/Routing.kt
+++ src/main/kotlin/cz/klement/plugins/Routing.kt
@@ -1,9 +1,9 @@
 package cz.klement.plugins
 
 import cz.klement.routes.users
-import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 
